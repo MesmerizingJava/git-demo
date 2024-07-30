eb13ffa7de29cb8324c8e0359d5986e442c24bc8
Paul Philips <biswanathmuduli738@gmail.com>
2024-07-30 23:03:15 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,14 @@
+package com.javatechie.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public void adddata(String data) {
+		System.out.println("data="+data);
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
