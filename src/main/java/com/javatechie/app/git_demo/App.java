24f149507b65a905f16a69a1c863d970a904c85b
Paul Philips <biswanathmuduli738@gmail.com>
2024-07-30 23:46:14 +0530

    added constant class and change in App.java

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -1,16 +1,13 @@
 package com.javatechie.app.git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public void adddata(String data) {
-		System.out.println("data="+data);
-		if(data.equals("java techie")) {
+
+		if (data.equals(Constant.DATA_TYPE)) {
 			System.out.println("welcome");
 		}
 	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
